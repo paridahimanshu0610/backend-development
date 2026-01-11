a34127221f65f135eabeb9913c69df29a6b635fe
paridahimanshu0610 <paridahimanshu0610@gmail.com>
2026-01-10 20:31:06 -0600

    helpful info

--- app/config.py
+++ app/config.py
@@ -1,5 +1,29 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+
+####################################################################################################################################
+# Important reading for setting environment variables: https://chatgpt.com/s/t_6962e79426a4819194633f55d9c553e7
+# pydantic-settings loads variables in this order:
+# 1. System environment variables (highest priority)
+# 2. Variables from env_file (if provided)
+# 3. Defaults (if any)
+
+# So if we deploy and set variables like:
+
+# ```
+# export DATABASE_HOSTNAME=db.example.com
+# export DATABASE_PASSWORD=supersecret
+# ```
+
+# Then those values will be used, even though we still have:
+# ```
+# env_file=".env"
+# ```
+
+# The `.env` file is simply ignored if the system variables exist.
+####################################################################################################################################
+
+
 # As per Pydantic version 1
 # class Settings(BaseSettings):
 #     database: str
